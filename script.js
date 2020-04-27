82a93722aec97fde8b45e5c34066b7d230c3c260
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-27 12:14:01 -0400

    super smelly calculator code with add/sub

---
+++ script.js
@@ -0,0 +1,43 @@
+
+// const firstNumber = document.querySelector('#firstInput');
+// console.log('this should be the first input');
+
+// const secondNumber = document.querySelector('#secondInput');
+// console.log('this should be the second input');
+
+// const firstNumberInput = firstNumber.value;
+// console.log(firstNumberInput, ' this should be the first input');
+
+// const secondNumberInput = secondNumber.value;
+
+
+
+
+const additionButtonClick = document.querySelector("#addition");
+additionButtonClick.addEventListener("click", function(){
+// consolelog('you clicked the addition button');
+const firstNumber = document.querySelector('#firstInput');
+const secondNumber = document.querySelector('#secondInput');
+let firstNumberInput = firstNumber.value;
+let secondNumberInput = secondNumber.value;
+let firstNumberInt = parseInt(firstNumberInput);
+let secondNumberInt = parseInt(secondNumberInput);
+document.querySelector("#output").innerHTML = firstNumberInt + secondNumberInt;
+firstNumber.value = "";
+secondNumber.value = "";
+});
+
+const subtractButtonClick = document.querySelector("#subtraction");
+subtractButtonClick.addEventListener("click", function(){
+// consolelog('you clicked the subtraction button');
+const firstNumber = document.querySelector('#firstInput');
+const secondNumber = document.querySelector('#secondInput');
+let firstNumberInput = firstNumber.value;
+let secondNumberInput = secondNumber.value;
+let firstNumberInt = parseInt(firstNumberInput);
+let secondNumberInt = parseInt(secondNumberInput);
+document.querySelector("#output").innerHTML = firstNumberInt - secondNumberInt;
+firstNumber.value = "";
+secondNumber.value = "";
+});
+
